70d469799d4351675a410e48c8178068bb8cab41
David <104325852+Cdaprod@users.noreply.github.com>
2023-12-14 18:35:03 -0500

    Add Union to typing import

--- services/chunks.py
+++ services/chunks.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple, Union
 import uuid
 import os
 from models.models import Document, DocumentChunk, DocumentChunkMetadata
