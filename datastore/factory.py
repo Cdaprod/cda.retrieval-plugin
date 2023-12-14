40831c203aae839d89ef8ee26b91bbc95de421d3
David <104325852+Cdaprod@users.noreply.github.com>
2023-12-14 18:24:36 -0500

    Add HybridDataStore class for integrating Weaviate and MinIO data stores
    Enhance Datastore Factory for Dynamic Instantiation
    
    - Added dynamic datastore instantiation in `factory.py` to support
    various datastores based on environment configuration.
    - Integrated new 'HybridDataStore' class to handle combined operations
    with MinIO and Weaviate datastores.
    - Implemented conditional instantiation for multiple datastore types,
    including MinIO, Weaviate, and other providers like Pinecone, Milvus,
    Redis, etc.
    - Removed duplicate 'weaviate' case in the match statement to
    streamline the selection logic.
    - Ensured the script's flexibility for future expansion with
    additional datastores.
    
    This update enables the application to easily switch between different
    data storage strategies, enhancing modularity and scalability.

--- datastore/factory.py
+++ datastore/factory.py
@@ -2,12 +2,11 @@ from datastore.datastore import DataStore
 from hybrid_datastore import HybridDataStore
 import os
 
-
 async def get_datastore() -> DataStore:
     datastore = os.environ.get("DATASTORE")
     assert datastore is not None
- 
-    match datastore: 
+
+    match datastore:
         case "minio":
             from minio_datastore import MinioDataStore
             return MinioDataStore()
@@ -22,56 +21,52 @@ async def get_datastore() -> DataStore:
 
         case "chroma":
             from datastore.providers.chroma_datastore import ChromaDataStore
-
             return ChromaDataStore()
+
         case "llama":
             from datastore.providers.llama_datastore import LlamaDataStore
-
             return LlamaDataStore()
 
         case "pinecone":
             from datastore.providers.pinecone_datastore import PineconeDataStore
-
             return PineconeDataStore()
-            
+
         case "milvus":
             from datastore.providers.milvus_datastore import MilvusDataStore
-
             return MilvusDataStore()
+
         case "zilliz":
             from datastore.providers.zilliz_datastore import ZillizDataStore
-
             return ZillizDataStore()
+
         case "redis":
             from datastore.providers.redis_datastore import RedisDataStore
-
             return await RedisDataStore.init()
+
         case "qdrant":
             from datastore.providers.qdrant_datastore import QdrantDataStore
-
             return QdrantDataStore()
+
         case "azuresearch":
             from datastore.providers.azuresearch_datastore import AzureSearchDataStore
-
             return AzureSearchDataStore()
+
         case "supabase":
             from datastore.providers.supabase_datastore import SupabaseDataStore
-
             return SupabaseDataStore()
+
         case "postgres":
             from datastore.providers.postgres_datastore import PostgresDataStore
-
             return PostgresDataStore()
+
         case "analyticdb":
             from datastore.providers.analyticdb_datastore import AnalyticDBDataStore
-
             return AnalyticDBDataStore()
-        case "elasticsearch":
-            from datastore.providers.elasticsearch_datastore import (
-                ElasticsearchDataStore,
-            )
 
-            return ElasticsearchDataStore() 
+        case "elasticsearch":
+            from datastore.providers.elasticsearch_datastore import ElasticsearchDataStore
+            return ElasticsearchDataStore()
+
         case _:
             raise ValueError(
                 f"Unsupported vector database: {datastore}. "
