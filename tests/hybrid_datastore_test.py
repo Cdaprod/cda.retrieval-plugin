1289dfa9ffc5c93694af84e7d586cecd73c19b6c
David <104325852+Cdaprod@users.noreply.github.com>
2023-12-14 21:25:45 -0500

    Refactor hybrid_datastore_test.py file

--- tests/hybrid_datastore_test.py
+++ tests/hybrid_datastore_test.py
@@ -2,17 +2,30 @@ import pytest
 from datastore.hybrid_datastore import HybridDataStore
 from models.models import Document, BucketObject, Bucket
 
-# Mocks for MinIO and Weaviate to simulate their behavior
 class MockMinIO:
     def __init__(self):
         self.bucket_name = "mock_bucket"
-        
+
     def upload_object(self, bucket_name, object_name, file_path):
         # Simulate upload functionality
         pass
 
     def list_objects(self, bucket_name):
-        return [BucketObject(id="1", object_name="sample.txt", object_type="text", size=1024)] 
+        # Including a 'path' attribute in the BucketObject
+        return [BucketObject(id="1", object_name="sample.txt", object_type="text", size=1024, path="mock/path/sample.txt")]
+
+# Test functions
+@pytest.mark.asyncio
+async def test_batch_ingest_from_bucket(hybrid_store):
+    result = await hybrid_store.batch_ingest_from_bucket("my_bucket")
+    assert "sample.txt" in result
+
+@pytest.mark.asyncio
+async def test_upsert(hybrid_store):
+    # Ensure the Document object includes a 'file_path' attribute
+    documents = [Document(id="doc1", text="Sample text", file_path="mock/path/doc1.txt")]
+    result = await hybrid_store.upsert(documents)
+    assert "doc1" in result
 
 class MockWeaviate:
     def upsert(self, documents):
@@ -25,14 +38,3 @@ def hybrid_store():
     store.minio_store = MockMinIO()
     store.weaviate_store = MockWeaviate()
     return store
-
-@pytest.mark.asyncio
-async def test_batch_ingest_from_bucket(hybrid_store):
-    result = await hybrid_store.batch_ingest_from_bucket("my_bucket")
-    assert "sample.txt" in result
-
-@pytest.mark.asyncio
-async def test_upsert(hybrid_store):
-    documents = [Document(id="doc1", text="Sample text")]
-    result = await hybrid_store.upsert(documents)
-    assert "doc1" in result
