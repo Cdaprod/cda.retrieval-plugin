971531cc2a4be7d1e8c997fbc856d4c6f9c37df8
David <104325852+Cdaprod@users.noreply.github.com>
2023-12-14 21:31:50 -0500

    Update bucket name in MockMinIO class

--- tests/hybrid_datastore_test.py
+++ tests/hybrid_datastore_test.py
@@ -4,14 +4,13 @@ from models.models import Document, BucketObject, Bucket
 
 class MockMinIO:
     def __init__(self):
-        self.bucket_name = "mock_bucket"
+        self.bucket_name = "my_bucket"
 
     def upload_object(self, bucket_name, object_name, file_path):
         # Simulate upload functionality
         pass
 
     def list_objects(self, bucket_name):
-        # Including a 'path' attribute in the BucketObject
         return [BucketObject(id="1", object_name="sample.txt", object_type="text", size=1024, path="mock/path/sample.txt")]
 
 # Test functions
@@ -22,10 +21,10 @@ async def test_batch_ingest_from_bucket(hybrid_store):
 
 @pytest.mark.asyncio
 async def test_upsert(hybrid_store):
-    # Ensure the Document object includes a 'file_path' attribute
     documents = [Document(id="doc1", text="Sample text", file_path="mock/path/doc1.txt")]
     result = await hybrid_store.upsert(documents)
     assert "doc1" in result
+
 
 class MockWeaviate:
     def upsert(self, documents):
